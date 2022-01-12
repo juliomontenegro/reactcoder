c7eb068cb51194931a038b9feae692e05c676761
juliomontenegro <jmontenegro0680@gmail.com>
2022-01-12 16:11:50 -0300

    Desafio clase 3

---
+++ src/Components/NavBar.js
@@ -0,0 +1,27 @@
+function Navbar (){  
+    return        <div>
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo">Tienda</a>
+        <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        <ul className="right hide-on-med-and-down">
+          <li><a href="#">producto1</a></li>
+          <li><a href="#">producto2</a></li>
+          <li><a href="#">producto3</a></li>
+          <li><a href="#">mi compra</a></li>
+        </ul>
+      </div>
+    </nav>
+    <ul className="sidenav" id="mobile-demo">
+      <li><a href="#">producto1</a></li>
+      <li><a href="#">producto2</a></li>
+      <li><a href="#">producto3</a></li>
+      <li><a href="#">mi compra</a></li>
+    </ul>
+  </div>
+  
+
+}
+
+
+export default Navbar;
